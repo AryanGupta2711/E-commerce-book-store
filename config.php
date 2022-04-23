5eeba195863ba7e262cb1aa1b96ee35d57223104
AryanGupta2711 <87652383+AryanGupta2711@users.noreply.github.com>
2022-04-23 23:07:16 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+
+$conn = mysqli_connect("localhost:3306/test","root","mysql","shop__db") or die('connection failed');
+
+?>
---
+++ footer.php
@@ -0,0 +1,33 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> 1800-200-200 </p>
+         <p> <i class="fas fa-phone"></i> 87234-34523 </p>
+         <p> <i class="fas fa-envelope"></i> allinonebooks@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> mumbai, india - 401034 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright <?php echo date('Y'); ?> <span></span> </p>
+
+</section>
